9d593406ec8abc052fee47e965305969f32561f4
Jonah Hendler <jhendle2@binghamton.edu>
2026-03-01 16:20:33 -0500

    FizzBuzz works

--- examples/FizzBuzz/FizzBuzz.c
+++ examples/FizzBuzz/FizzBuzz.c
@@ -1,5 +1,13 @@
 void fizzBuzz(const int n) {
     for (int i = 0; i < n; i++) {
-        if (i % 3 == 0) printf("Fizz\n");
+        if      (i %  3 == 0) printf("Fizz\n");
+        else if (i %  5 == 0) printf("Buzz\n");
+        else if (i % 15 == 0) printf("FizzBuzz\n");
+        else printf("%d\n", i);
     }
+}
+
+int main(void) {
+    fizzBuzz(20);
+    return 0;
 }
---
+++ examples/If/While.c
@@ -0,0 +1,7 @@
+int main() {
+    int n = 5;
+    int i = 0;
+    while (i < n) {
+        i++;
+    }
+}
